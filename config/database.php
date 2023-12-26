70490ecfdb2960b6577a7c7cd9f703303bb3994f
manish batham <manishbatham@manishs-MacBook-Air.local>
2023-12-26 12:35:21 +0530

    updated env vars and database config

--- config/database.php
+++ config/database.php
@@ -62,6 +62,26 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'mysql2' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host2' => env('DB_HOST', '127.0.0.1'),
+            'port2' => env('DB_PORT', '3306'),
+            'database2' => env('DB_DATABASE', 'forge'),
+            'username2' => env('DB_USERNAME', 'forge'),
+            'password2' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
